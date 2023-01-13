4e63e30aac05fd58a7dedc187a8cb4edef33fb8e
bora <youonl5@naver.com>
2023-01-13 16:51:04 +0900

    feature: add getUserId() 임시 함수

---
+++ src/main/java/com/gym/utils/JwtService.java
@@ -0,0 +1,14 @@
+package com.gym.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    public Integer getUserId() {
+        Integer userId = 0;
+        return userId;
+    }
+}
