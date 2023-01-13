326c6a8bb2fa277559b37a04df53ac14bca87a03
Sewoong <lympsw12@naver.com>
2023-01-14 01:22:58 +0900

    fetch / Record save

--- src/main/java/com/gym/record/RecordRepository.java
+++ src/main/java/com/gym/record/RecordRepository.java
@@ -1,6 +1,11 @@
 package com.gym.record;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecordRepository extends JpaRepository<Record, Integer> {
+
+   @Query("select r from Record r where r.user.userId =: userId and date_format(r.createdAt, '%Y-%m-%d') =: date")
+   Record findRecordMonth(@Param("date") String date, @Param("userId") int userId);
 }
