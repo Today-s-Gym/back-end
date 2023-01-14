8663a06408a44b0c682bf0214f78e7f1876b94d6
Sewoong <lympsw12@naver.com>
2023-01-14 15:05:19 +0900

    feat / Record, Img, Tag insert together

--- src/main/java/com/gym/record/RecordController.java
+++ src/main/java/com/gym/record/RecordController.java
@@ -2,6 +2,9 @@ package com.gym.record;
 
 
 import com.gym.record.photo.RecordPhoto;
+import com.gym.record.photo.RecordPhotoService;
+import com.gym.tag.Tag;
+import com.gym.tag.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +17,28 @@ import java.util.List;
 public class RecordController {
 
     private final RecordService recordService;
+    private final RecordPhotoService recordPhotoService;
+    private final RecordRepository recordRepository;
+    private final TagService tagService;
 
     /**
-     * Record 저장
+     * Record, 사진여러개, 태그여러개 저장
      */
     @PostMapping("/record")
-    public Integer createRecord(@RequestBody RecordRequestDto recordRequestDto){
-        System.out.println(recordRequestDto.getContent());
-        return recordService.saveRecord(recordRequestDto.getContent());
+    public void createRecord(@RequestBody RecordRequestDto recordRequestDto){
+        Integer recordId = recordService.saveRecord(recordRequestDto.getContent());
+        Record record = recordRepository.findById(recordId).get();
+        List<RecordPhoto> recordPhotos = recordRequestDto.getRecordPhotos();
+        for (RecordPhoto recordPhoto : recordPhotos) {
+            recordPhoto.createRecord(record);
+        }
+        List<Tag> tags = recordRequestDto.getTags();
+        for (Tag tag : tags) {
+            tag.createRecord(record);
+            tag.createUser(record.getUser());
+        }
+        recordPhotoService.saveRecordPhoto(recordPhotos);
+        tagService.saveTag(tags);
     }
 
 
