d2abd0cec5bcd8ece83308a3d4f01ddc111051c2
bora <youonl5@naver.com>
2023-01-14 23:00:36 +0900

    feat: 좋아요 누르기

---
+++ src/main/java/com/gym/post/like/dto/PushLikeRes.java
@@ -0,0 +1,16 @@
+package com.gym.post.like.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class PushLikeRes {
+
+    private Integer userId;
+    private Integer postId;
+    private String result;
+}
