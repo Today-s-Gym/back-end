a0e6414e257539014850b5dffd6c70fa4dd55cb5
bora <youonl5@naver.com>
2023-01-14 22:33:48 +0900

    feat: 특정 게시판 모든 게시글 조회

--- src/main/java/com/gym/post/PostRepository.java
+++ src/main/java/com/gym/post/PostRepository.java
@@ -1,6 +1,14 @@
 package com.gym.post;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    @Query(value = "SELECT p FROM Post p WHERE p.category= :categoryId ORDER BY p.postId DESC")
+    Optional<List<Post>> findByCategoryId(Integer categoryId, Pageable limit);
 }
--- src/main/java/com/gym/user/UserRepository.java
+++ src/main/java/com/gym/user/UserRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
+    Optional<User> getByUserId(Integer userId);
 }
