96f2924b555576b9f8854129ab7040a7557a9c06
EUNJEONGMUN <munej26@gmail.com>
2023-01-14 13:35:36 +0900

    fix: AccountPrivacyReq 기본 생성자 추가

--- src/main/java/com/gym/user/dto/AccountPrivacyReq.java
+++ src/main/java/com/gym/user/dto/AccountPrivacyReq.java
@@ -2,8 +2,10 @@ package com.gym.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class AccountPrivacyReq {
     private boolean locked;
