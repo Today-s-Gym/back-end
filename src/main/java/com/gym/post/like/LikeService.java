0d15b3938338919e56763e7e9bed197d11d338d8
bora <youonl5@naver.com>
2023-01-14 23:00:36 +0900

    feat: 좋아요 누르기

--- src/main/java/com/gym/post/like/LikeService.java
+++ src/main/java/com/gym/post/like/LikeService.java
@@ -1,13 +1,34 @@
 package com.gym.post.like;
 
+import com.gym.config.exception.BaseException;
+import com.gym.post.Post;
+import com.gym.post.like.dto.PushLikeRes;
+import com.gym.utils.UtilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
 
     private final LikeRepository likeRepository;
+    private final UtilService utilService;
+
+    @Transactional
+    public void save(Like like) {
+        likeRepository.save(like);
+    }
+
+    public void createLike(Integer userId, Integer postId) {
+        Like like = Like.builder()
+                .userId(userId)
+                .postId(postId)
+                .status(true)
+                .build();
+        save(like);
+    }
 
     public boolean checkLike(Integer userId, Integer postId) {
         Like like = likeRepository.getLikeByUserIdAndCardId(userId, postId).orElse(null);
@@ -24,5 +45,24 @@ public class LikeService {
         return likeRepository.getLikeCountsByPostId(postId).orElse(0);
     }
 
+
+    @Transactional
+    public PushLikeRes pushLike(Integer userId, Integer postId) throws BaseException {
+        utilService.findByPostIdWithValidation(postId);
+        Like like = likeRepository.getLikeByUserIdAndCardId(userId, postId).orElse(null);
+
+        String result = "";
+        // 새로 누르는 좋아요
+        if (like == null) {
+            createLike(userId, postId);
+            result = "좋아요를 누름";
+        }
+        // 좋아요 상태 변경
+        else {
+            like.changeStatus();
+            result = "좋아요 상태 변경 성공";
+        }
+        return new PushLikeRes(userId, postId, result);
+    }
 
 }
