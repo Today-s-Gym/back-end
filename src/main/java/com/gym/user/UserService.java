5fa3f4c944dedfcd077027f3449a283d3be54d33
EUNJEONGMUN <munej26@gmail.com>
2023-01-15 13:40:36 +0900

    feat: 사용자 이메일 조회 기능

--- src/main/java/com/gym/user/UserService.java
+++ src/main/java/com/gym/user/UserService.java
@@ -1,5 +1,7 @@
 package com.gym.user;
 
+import com.gym.config.exception.BaseException;
+import com.gym.utils.UtilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UtilService utilService;
 
     /**
      * 사용자 공개 계정 전환
@@ -18,5 +21,14 @@ public class UserService {
         User user = userRepository.findById(userId).get();
         user.changeAccountPrivacy(locked);
         return user.getUserId();
+    }
+
+    /**
+     * 사용자 이메일 조회
+     */
+    @Transactional
+    public String findUserEmailByUserId(Integer userId) throws BaseException {
+        User user = utilService.findByUserIdWithValidation(userId);
+        return user.getEmail();
     }
 }
