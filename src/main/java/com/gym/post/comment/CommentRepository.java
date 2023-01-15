4b0660ad21e48d7f17a190c83479d1be2dc2cc2c
EUNJEONGMUN <munej26@gmail.com>
2023-01-15 11:46:59 +0900

    feat: 유효한 댓글 가져오기 구현

--- src/main/java/com/gym/post/comment/CommentRepository.java
+++ src/main/java/com/gym/post/comment/CommentRepository.java
@@ -2,5 +2,8 @@ package com.gym.post.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    Optional<Comment> getByCommentId(Integer commentId);
 }
