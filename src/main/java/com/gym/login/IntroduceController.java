57003f836073138a290060a59f0d7d3f6ae4d25a
dlswns <dlswns8679@naver.com>
2023-01-24 10:53:27 +0900

    feat/try-catch문 포함

--- src/main/java/com/gym/login/IntroduceController.java
+++ src/main/java/com/gym/login/IntroduceController.java
@@ -1,10 +1,12 @@
 package com.gym.login;
 
 
+import com.gym.tag.config.exception.BaseException;
 import com.gym.tag.config.exception.BaseResponse;
 import com.gym.tag.config.exception.BaseResponseStatus;
 import com.gym.user.User;
 import com.gym.user.UserRepository;
+import com.gym.utils.UtilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,28 +15,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Controller
 public class IntroduceController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UtilService utilService;
+
     @PutMapping("/login/introduce")
     @ResponseBody
-    public BaseResponse<?> updateIntroduce(@RequestBody User user, @RequestParam("nickname") String nickname, @RequestParam("introduce") String introduce)
+    public BaseResponse<?> updateIntroduce(@RequestParam("userid") int userid, @RequestParam("nickname") String nickname, @RequestParam("introduce") String introduce)
     {
+        try{
+            User user = utilService.findByUserIdWithValidation(userid);
+            if((introduce.length() >= 0) && (introduce.length() <= 30)){
+                user.setNickName(nickname);
+                user.setIntroduce(introduce);
+                userRepository.save(user);
+                return new BaseResponse<>(BaseResponseStatus.SUCCESS);
 
-        if((introduce.length() >= 0) && (introduce.length() <= 30)){
-            user.setNickName(nickname);
-            user.setIntroduce(introduce);
-            userRepository.save(user);
-            return new BaseResponse<>(BaseResponseStatus.SUCCESS);
+            }
+            else{
+                return new BaseResponse<>(BaseResponseStatus.INTRODUCE_ERROR);
 
+            }
         }
-        else{
-            return new BaseResponse<>(BaseResponseStatus.INTRODUCE_ERROR);
-
+        catch(BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
         }
+
     }
 }
--- src/main/java/com/gym/login/SportsController.java
+++ src/main/java/com/gym/login/SportsController.java
@@ -2,32 +2,41 @@ package com.gym.login;
 
 
 import com.gym.category.Category;
+import com.gym.tag.config.exception.BaseException;
 import com.gym.tag.config.exception.BaseResponse;
 import com.gym.tag.config.exception.BaseResponseStatus;
 import com.gym.user.User;
 import com.gym.user.UserRepository;
+import com.gym.utils.UtilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class SportsController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UtilService utilService;
+
     @PutMapping("/login/sports")
     @ResponseBody
-    public BaseResponse<?> uploadSports(@RequestBody User user, @RequestParam("categoryid") int categoryid)
+    public BaseResponse<?> uploadSports(@RequestParam("userid") int userid, @RequestParam("categoryid") int categoryid)
+
     {
-
-        Category category = new Category();
-        category.setCategoryId(categoryid);
-        user.setCategory(category);
-        userRepository.save(user);
-        return new BaseResponse<>(BaseResponseStatus.SUCCESS);
-
+        try{
+            User user = utilService.findByUserIdWithValidation(userid);
+            Category category = new Category();
+            category.setCategoryId(categoryid);
+            user.setCategory(category);
+            userRepository.save(user);
+            return new BaseResponse<>(BaseResponseStatus.SUCCESS);
+        } catch(BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
     }
 
 
