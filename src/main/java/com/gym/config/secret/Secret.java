f9371f638d3a2b46bde120d192494ea0da83cd66
EUNJEONGMUN <munej26@gmail.com>
2023-01-24 11:22:52 +0900

    feat: JWT에서 userId 추출

--- src/main/java/com/gym/config/secret/Secret.java
+++ src/main/java/com/gym/config/secret/Secret.java
@@ -1,4 +1,6 @@
 package com.gym.config.secret;
 
 public class Secret {
+    public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nu";
+
 }
--- src/main/java/com/gym/utils/JwtService.java
+++ src/main/java/com/gym/utils/JwtService.java
@@ -1,7 +1,19 @@
 package com.gym.utils;
 
+import com.gym.config.exception.BaseException;
+import com.gym.config.secret.Secret;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.gym.config.exception.BaseResponseStatus.EMPTY_JWT;
+import static com.gym.config.exception.BaseResponseStatus.INVALID_JWT;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +22,37 @@ public class JwtService {
     public static Integer getUserId() {
         Integer userId = 1;
         return userId;
+    }
+
+    /**
+     * Header에서 Authorization 으로 JWT 추출
+     */
+    public String getJwt(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return request.getHeader("Authorization");
+    }
+
+    /*
+    JWT에서 userId 추출
+     */
+    public int getUserIdx() throws BaseException {
+        //1. JWT 추출
+        String accessToken = getJwt();
+        if(accessToken == null || accessToken.length() == 0){
+            throw new BaseException(EMPTY_JWT);
+        }
+
+        // 2. JWT parsing
+        Jws<Claims> claims;
+        try{
+            claims = Jwts.parser()
+                    .setSigningKey(Secret.JWT_SECRET_KEY)
+                    .parseClaimsJws(accessToken);
+        } catch (Exception ignored) {
+            throw new BaseException(INVALID_JWT);
+        }
+
+        // 3. userId 추출
+        return claims.getBody().get("userId",Integer.class);
     }
 }
