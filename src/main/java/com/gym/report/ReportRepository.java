f3ec17d62c895b3af30ebb89614105c0dffb746f
EUNJEONGMUN <munej26@gmail.com>
2023-01-31 19:48:38 +0900

    feat: 신고 중복 확인 메서드 추가

--- src/main/java/com/gym/report/ReportRepository.java
+++ src/main/java/com/gym/report/ReportRepository.java
@@ -1,6 +1,19 @@
 package com.gym.report;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Integer> {
+
+    @Query("select r from Report r where r.reporterId =:reporterId and r.reportedId =:reportedId and r.type = 'USER'")
+    Optional<Report> findUserReport(@Param("reporterId") Integer reporterId, @Param("reportedId") Integer reportedId);
+
+    @Query("select r from Report r where r.reporterId =:reporterId and r.reportedId =:reportedId and r.type = 'POST'")
+    Optional<Report> findPostReport(@Param("reporterId") Integer reporterId, @Param("reportedId") Integer reportedId);
+
+    @Query("select r from Report r where r.reporterId =:reporterId and r.reportedId =:reportedId and r.type = 'COMMENT'")
+    Optional<Report> findCommentReport(@Param("reporterId") Integer reporterId, @Param("reportedId") Integer reportedId);
 }
