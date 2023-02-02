fd623735f4e6d42aac7bc97403b7e30e410f9ffe
EUNJEONGMUN <munej26@gmail.com>
2023-02-02 12:18:05 +0900

    test: 아바타 레벨업 테스트 - MyAvatarColleciton 반영여부 확인

--- src/test/java/com/gym/avatar/AvatarServiceTest.java
+++ src/test/java/com/gym/avatar/AvatarServiceTest.java
@@ -1,8 +1,13 @@
 package com.gym.avatar;
 
 import com.gym.avatar.avatar.AvatarStep;
+import com.gym.avatar.myAvatarCollection.MyAvatarCollectionRepository;
+import com.gym.category.Category;
+import com.gym.config.exception.BaseException;
 import com.gym.user.User;
 import com.gym.user.UserRepository;
+import com.gym.user.UserService;
+import com.gym.user.dto.GetMyPageRes;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +19,48 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class AvatarServiceTest {
-    @Autowired
-    private UserRepository userRepository;
+    @Autowired UserRepository userRepository;
     @Autowired AvatarService avatarService;
+    @Autowired UserService userService;
+
 
     @Test
-    void setInitialAvatar() {
-        User user = new User();
-        user.update("test_user", "안녕");
+    void setInitialAvatar() throws BaseException {
+        User user = createTestUser();
         userRepository.save(user);
         avatarService.setInitialAvatar(user);
 
         Assertions.assertThat(user.getMyAvatar().getAvatarStep()).isEqualTo(AvatarStep.getInitialAvatarStep());
+        GetMyPageRes myPage = userService.getMyPage(user.getUserId());
+        Assertions.assertThat(myPage)
+                .extracting("avatarImgUrl")
+                .isEqualTo(AvatarStep.getInitialAvatarStep().getImgUrl());
+    }
+
+    @Test
+    void userLevelUpTest() throws BaseException {
+        User user = createTestUser();
+        userRepository.save(user);
+        avatarService.setInitialAvatar(user);
+
+        AvatarStep levelUpAvatar = AvatarStep.AVATAR1_STEP2;
+        avatarService.userLevelUp(user, levelUpAvatar);
+
+        Assertions.assertThat(user.getMyAvatar().getAvatarStep()).isEqualTo(levelUpAvatar);
+        GetMyPageRes myPage = userService.getMyPage(user.getUserId());
+        Assertions.assertThat(myPage)
+                .extracting("avatarImgUrl")
+                .isEqualTo(AvatarStep.AVATAR1_STEP2.getImgUrl());
+    }
+
+    private User createTestUser() {
+        User user = new User();
+        user.update("test_user", "안녕");
+
+        Category category = new Category();
+        category.setCategoryId(1);
+        user.updateSports(category);
+
+        return user;
     }
 }
