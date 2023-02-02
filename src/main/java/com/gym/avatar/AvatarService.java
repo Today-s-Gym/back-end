478d5275639a32034c80f9c95984a0765e17e6a6
EUNJEONGMUN <munej26@gmail.com>
2023-02-02 12:16:59 +0900

    refactor: 로그 추가

--- src/main/java/com/gym/avatar/AvatarService.java
+++ src/main/java/com/gym/avatar/AvatarService.java
@@ -8,9 +8,11 @@ import com.gym.avatar.myAvatarCollection.MyAvatarCollectionRepository;
 import com.gym.user.User;
 import com.gym.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AvatarService {
@@ -20,19 +22,23 @@ public class AvatarService {
 
     @Transactional
     public boolean checkAndMyAvatarLevelUp(Integer userId) {
+        log.info("레벨업 확인 user : {}", userId);
         User user = userRepository.findWithMyAvatarByUserId(userId);
         int recordCount = user.getRecordCount();
         AvatarStep avatarStep = AvatarStep.findByRecordCount(recordCount);
 
         if (!user.getMyAvatar().getAvatarStep().equals(avatarStep)) {
+            log.info("레벨업 조건 만족 : {}", avatarStep);
             userLevelUp(user, avatarStep);
             return true;
         }
+        log.info("레벨업 조건 미달");
         return false;
     }
 
     @Transactional
     public void setInitialAvatar(User user) {
+        log.info("최초 로그인 - 초기 아바타 설정");
         MyAvatar initialMyAvatar = getInitialMyAvatar();
         changeAvatarStep(user, initialMyAvatar);
     }
